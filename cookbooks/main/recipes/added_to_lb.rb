c9f2568150831a0960c45f20da480e1c383a11cd
Drew Delianides <drew@delianides.com>
2013-08-22 16:31:36 -0400

    added ips to lb scripts

--- cookbooks/main/recipes/added_to_lb.rb
+++ cookbooks/main/recipes/added_to_lb.rb
@@ -2,15 +2,11 @@ rightscale_marker :begin
 
 include_recipe "hipchat"
 
-log" HOSTNAME: #{node[:cloud][:hostname]}"
-log" SERVERNAME: #{node[:cloud][:server_name]}"
-log" NAME: #{node[:cloud][:name]}"
-
 hipchat_msg "added to lb" do
   room "Web Team"
   token node[:hipchat][:api_key]
   nickname "RightScale"
-  message "Instance: #{node[:cloud][:hostname]}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
+  message "Instance: #{node[:cloud][:public_ip]}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
   color "purple"
 end
 
