58ebc7924587e8acc1c79863af3a3b5415d88075
Drew Delianides <drew@delianides.com>
2013-08-22 11:36:59 -0400

    trying to install hipchat

--- cookbooks/hipchat/recipes/default.rb
+++ cookbooks/hipchat/recipes/default.rb
@@ -1,6 +1,10 @@
-hipchat = gem_package 'hipchat' do
-  action :nothing
-  version "0.11.0"
+#hipchat = gem_package 'hipchat' do
+#  action :nothing
+#  version "0.11.0"
+#end
+#
+#hipchat.run_action(:install)
+#
+execute "Install HipChat API" do
+  command "gem install hipchat"
 end
-
-hipchat.run_action(:install)
