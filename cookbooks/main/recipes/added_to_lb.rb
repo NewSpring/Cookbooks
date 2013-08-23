a30a7d0e80dbdee5af4de221eb0214ad583335c5
Drew Delianides <drew@delianides.com>
2013-08-23 14:41:28 -0400

    trying to get hostname in hipchat

--- cookbooks/main/recipes/added_to_lb.rb
+++ cookbooks/main/recipes/added_to_lb.rb
@@ -2,11 +2,20 @@ rightscale_marker :begin
 
 include_recipe "hipchat"
 
+# Logs 'node[:rightscale][:servers][:sketchy][:hostname]'.
+# Raises an Exception if it isn't set.
+if node[:rightscale][:servers][:sketchy][:hostname].to_s.empty?
+  raise "'node[:rightscale][:servers][:sketchy][:hostname]' must be set!"
+else
+  log "  Sketchy hostname: #{node[:rightscale][:servers][:sketchy][:hostname]}"
+  hostname = node[:rightscale][:servers][:sketchy][:hostname]
+end
+
 hipchat_msg "added to lb" do
   room "Web Team"
   token node[:hipchat][:api_key]
   nickname "RightScale"
-  message "Instance: #{node[:cloud][:public_ip]}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
+  message "Instance: #{hostname}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
   color "green"
 end
 
