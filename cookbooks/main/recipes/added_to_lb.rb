b30a02dd0dc32078584cc3ba0307471ab778d5bb
Drew Delianides <drew@delianides.com>
2013-08-23 23:44:56 -0400

    oops...should have been public IP

--- cookbooks/main/recipes/added_to_lb.rb
+++ cookbooks/main/recipes/added_to_lb.rb
@@ -6,7 +6,7 @@ hipchat_msg "added to lb" do
   room "Web Team"
   token node[:hipchat][:api_key]
   nickname "RightScale"
-  message "Instance: #{node[:cloud][:private_ips][0]}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
+  message "Instance: #{node[:cloud][:public_ips][0]}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
   color "green"
 end
 
--- cookbooks/main/recipes/removed_from_lb.rb
+++ cookbooks/main/recipes/removed_from_lb.rb
@@ -10,7 +10,7 @@ hipchat_msg "added to lb" do
   room "Web Team"
   token node[:hipchat][:api_key]
   nickname "RightScale"
-  message "Instance: #{node[:cloud][:private_ips][0]}, has been removed from Load Balancer: #{node[:lb][:service][:lb_name]}"
+  message "Instance: #{node[:cloud][:public_ips][0]}, has been removed from Load Balancer: #{node[:lb][:service][:lb_name]}"
   color "purple"
 end
 
