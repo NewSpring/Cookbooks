39f57c3137e347eba02f9063ccb44311f20852a8
Drew Delianides <drew@delianides.com>
2013-08-23 23:02:24 -0400

    instance for hipchat is now the public ip

--- cookbooks/main/recipes/added_to_lb.rb
+++ cookbooks/main/recipes/added_to_lb.rb
@@ -2,20 +2,11 @@ rightscale_marker :begin
 
 include_recipe "hipchat"
 
-# Logs 'node[:rightscale][:servers][:sketchy][:hostname]'.
-# Raises an Exception if it isn't set.
-if node[:rightscale][:servers][:sketchy][:hostname].to_s.empty?
-  raise "'node[:rightscale][:servers][:sketchy][:hostname]' must be set!"
-else
-  log "  Sketchy hostname: #{node[:cloud].inspect}"
-  hostname = node[:rightscale][:servers][:sketchy][:hostname]
-end
-
 hipchat_msg "added to lb" do
   room "Web Team"
   token node[:hipchat][:api_key]
   nickname "RightScale"
-  message "Instance: #{hostname}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
+  message "Instance: #{node[:cloud][:private_ips][0]}, has been added to Load Balancer: #{node[:lb][:service][:lb_name]}"
   color "green"
 end
 
