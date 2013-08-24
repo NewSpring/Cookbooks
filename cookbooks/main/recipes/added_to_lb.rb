9609d076d866e52ab2ac265eba57023eaa346a4b
Drew Delianides <drew@delianides.com>
2013-08-23 22:23:19 -0400

    inspecting cloud

--- cookbooks/main/recipes/added_to_lb.rb
+++ cookbooks/main/recipes/added_to_lb.rb
@@ -7,7 +7,7 @@ include_recipe "hipchat"
 if node[:rightscale][:servers][:sketchy][:hostname].to_s.empty?
   raise "'node[:rightscale][:servers][:sketchy][:hostname]' must be set!"
 else
-  log "  Sketchy hostname: #{node[:rightscale][:servers].inspect}"
+  log "  Sketchy hostname: #{node[:cloud].inspect}"
   hostname = node[:rightscale][:servers][:sketchy][:hostname]
 end
 
