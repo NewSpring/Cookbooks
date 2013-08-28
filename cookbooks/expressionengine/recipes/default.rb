2dc0dd913b099fd2787bacdeabcd40761c453e59
Drew Delianides <drew@delianides.com>
2013-08-28 16:17:00 -0400

    fixed template variables

--- cookbooks/expressionengine/recipes/default.rb
+++ cookbooks/expressionengine/recipes/default.rb
@@ -51,7 +51,7 @@ end
 #Make sure EE permissions are correct
 bash "set_permissions" do
  user "root"
- cwd "#{node[:repo][:default][:destination]}/"
+ cwd "#{node[:repo][:default][:destination]}/#{node[:ee][:main]}"
  code <<-EOH
    chmod -R 777 hello/expressionengine/cache
    chmod -R 777 assets/cache
