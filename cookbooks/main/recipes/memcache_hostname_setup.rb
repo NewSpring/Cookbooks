2af2dfb5436a95bd9030faea7b71ce34d58fafe4
Drew Delianides <drew@delianides.com>
2013-09-04 16:22:27 -0400

    more memcache setup

--- cookbooks/main/recipes/memcache_hostname_setup.rb
+++ cookbooks/main/recipes/memcache_hostname_setup.rb
@@ -1,8 +1,11 @@
+rightscale_marker
+
 include_recipe "rightscale::default"
 
-memcache = rightscale_server_collection "memcache_servers" do
+rightscale_server_collection "memcache_servers" do
   tags "memcached_server:active=true"
   mandatory_tags "server:private_ip_0=*"
+  action :load
 end
 
-log "This is a log for memcache server #{memcache.inspect}"
+log "This is a log for memcache server #{node[:server_collection].inspect}"
