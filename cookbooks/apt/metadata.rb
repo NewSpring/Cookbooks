2a3313400d0e9d54aaea4369e17f54c128707490
Drew Delianides <drew@delianides.com>
2013-09-04 11:38:51 -0400

    added ::default to metadata

--- cookbooks/apt/metadata.rb
+++ cookbooks/apt/metadata.rb
@@ -5,7 +5,7 @@ license           "Apache 2.0"
 description       "Configures apt and apt services and LWRPs for managing apt repositories and preferences"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.9.3"
-recipe            "apt", "Runs apt-get update during compile phase and sets up preseed directories"
+recipe            "apt::default", "Runs apt-get update during compile phase and sets up preseed directories"
 recipe            "apt::cacher-ng", "Set up an apt-cacher-ng caching proxy"
 recipe            "apt::cacher-client", "Client for the apt::cacher-ng caching proxy"
 
