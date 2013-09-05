f55cf448f238bd5ee464ffbaf1db9edaca3b9817
Drew Delianides <drew@delianides.com>
2013-09-04 22:37:25 -0400

    trying a different method

--- cookbooks/main/recipes/memcache_hostname_setup.rb
+++ cookbooks/main/recipes/memcache_hostname_setup.rb
@@ -8,8 +8,17 @@ rightscale_server_collection search do
   action :load
 end
 
-tags = node[:server_collection][search]
-log "The tags are: #{tags}"
+ip_list = []
+valid_ip_regex =
+            '\b(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}' +
+            '([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b'
+
+ip_list = node[:server_collection][search].collect do |_, tags|
+  RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags, valid_ip_regex)
+end
+
+log "The tags are: #{ip_list[0]}"
+
 
 
 #memcache_server_ip = RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags)
