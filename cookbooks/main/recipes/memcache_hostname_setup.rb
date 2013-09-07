daae1745ebde6b3c88fbde973592f876386731b3
Drew Delianides <drew@delianides.com>
2013-09-06 23:00:10 -0400

    had to run the action afterwards

--- cookbooks/main/recipes/memcache_hostname_setup.rb
+++ cookbooks/main/recipes/memcache_hostname_setup.rb
@@ -9,25 +9,23 @@ end
 
 collection.run_action(:load)
 
-log "Tags: #{node[:server_collection][search].inspect}"
+ruby_block "Installing Memcache Host..." do
 
-#ruby_block "Installing Memcache Host..." do
-#
-#  ip_list = []
-#  valid_ip_regex =
-#              '\b(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}' +
-#              '([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b'
-#
-#  ip_list = node[:server_collection][search].collect do |_, tags|
-#    RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags, valid_ip_regex)
-#  end
-#
-#  block do
-#    file = Chef::Util::FileEdit.new("/etc/hosts")
-#    file.insert_line_if_no_match(
-#      "# Memcache Private Server IP",
-#      "\n# Memcache Private Server IP\n#{ip_list[0]}     memcache_host"
-#    )
-#    file.write_file
-#  end
-#end
+  ip_list = []
+  valid_ip_regex =
+              '\b(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}' +
+              '([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b'
+
+  ip_list = node[:server_collection][search].collect do |_, tags|
+    RightScale::Utils::Helper.get_tag_value("server:private_ip_0", tags, valid_ip_regex)
+  end
+
+  block do
+    file = Chef::Util::FileEdit.new("/etc/hosts")
+    file.insert_line_if_no_match(
+      "# Memcache Private Server IP",
+      "\n# Memcache Private Server IP\n#{ip_list[0]}     memcache_host"
+    )
+    file.write_file
+  end
+end
