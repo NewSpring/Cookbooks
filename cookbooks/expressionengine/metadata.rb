ce6effe8c0a25096b693992482b25004d285c867
Drew Delianides <drew@delianides.com>
2013-09-09 12:13:15 -0400

    updated expressionengine cookbook

--- cookbooks/expressionengine/metadata.rb
+++ cookbooks/expressionengine/metadata.rb
@@ -6,6 +6,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.7"
 
 depends "apache2"
+depends "repo"
 
 recipe "expressionengine::default", "Installs the Expressione Engine System and Clones down the repo"
 recipe "expressionengine::multisite", "Installs and Configures EE MSM Sites"
