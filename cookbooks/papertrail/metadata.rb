2271ec4a11f185285322a1a790e31241a6c5553c
Drew Delianides <drew@delianides.com>
2013-09-17 15:45:53 -0400

    removed remote_syslog for papertrail service

---
+++ cookbooks/papertrail/metadata.rb
@@ -0,0 +1,45 @@
+maintainer       "First Banco"
+maintainer_email "rob@firstbanco.com"
+license          "All rights reserved"
+description      "Installs/Configures papertrail"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends         "build-essential"
+depends          "rsyslog"
+
+supports "ubuntu"
+supports "debian"
+
+recipe "papertrail::default", "Installs the Configuration to send logs to the papertrail app service"
+
+attribute "papertrail/logs",
+  :display_name => "Log Files",
+  :description => "Select with log files to send",
+  :required => "required",
+  :type => "array",
+  :recipes => [
+    "papertrail::default"
+  ]
+
+  attribute "papertrail/host",
+  :display_name => "Papertrail Host",
+  :description => "Hostname for Papertrail Service",
+  :default => "logs.papertrailapp.com",
+  :recipes => [
+    "papertrail::default"
+  ]
+
+  attribute "papertrail/port",
+  :display_name => "Papertrail Port",
+  :description => "Port for Papertrail Service",
+  :required => "required",
+  :recipes => [
+    "papertrail::default"
+  ]
+
+  attribute "papertrail/exclude_patterns",
+  :display_name => "Exclude Patterns",
+  :description => "Pattern Regex to Exclude",
+  :recipes => [
+    "papertrail::default"
+  ]
--- cookbooks/remote_syslog/recipes/gem.rb
+++
@@ -1,5 +0,0 @@
-remote_syslog = gem_package "remote_syslog" do
-  action :nothing
-end
-
-remote_syslog.run_action(:install)
