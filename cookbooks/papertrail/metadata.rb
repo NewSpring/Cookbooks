e0809767ac5b782ce42aa45e527f930ecd33da0d
Drew Delianides <drew@delianides.com>
2013-09-17 16:30:36 -0400

    updated meta

--- cookbooks/papertrail/metadata.rb
+++ cookbooks/papertrail/metadata.rb
@@ -5,7 +5,6 @@ description      "Installs/Configures papertrail"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends         "build-essential"
-depends          "rsyslog"
 
 supports "ubuntu"
 supports "debian"
