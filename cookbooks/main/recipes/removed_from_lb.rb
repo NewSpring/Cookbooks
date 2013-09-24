71f8afd491a45e9b7a3de99fabba3751903aab19
Drew Delianides <drew@delianides.com>
2013-09-23 21:24:28 -0400

    added input to remove input

--- cookbooks/main/recipes/removed_from_lb.rb
+++ cookbooks/main/recipes/removed_from_lb.rb
@@ -7,7 +7,7 @@ log" SERVERNAME: #{node[:cloud][:server_name]}"
 log" NAME: #{node[:cloud][:name]}"
 
 hipchat_msg "added to lb" do
-  room "Web Team"
+  room node[:hipchat][:room]
   token node[:hipchat][:api_key]
   nickname "RightScale"
   message "Instance: #{node[:cloud][:public_ips][0]}, has been removed from Load Balancer: #{node[:lb][:service][:lb_name]}"
