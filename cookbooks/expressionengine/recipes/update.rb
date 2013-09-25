1e263e9ff7beb0197f1edd71777d457e1173dd0a
Drew Delianides <drew@delianides.com>
2013-09-25 10:48:23 -0400

    changed to deploy resource

--- cookbooks/expressionengine/recipes/update.rb
+++ cookbooks/expressionengine/recipes/update.rb
@@ -14,52 +14,54 @@ include_recipe "repo::default"
 
 site_install_dir = "#{node[:repo][:default][:destination]}/#{node[:ee][:main]}"
 
-repo node[:web_apache][:application_name] do
-  destination "#{node[:repo][:default][:destination]}/#{node[:ee][:main]}"
-  symlinks "images" => "images", "css" => "assets/css"
+deploy_revision site_install_dir do
+  symlinks ( "images" => "images", "css" => "assets/css" )
   action node[:repo][:default][:perform_action].to_sym
   credential node[:repo][:default][:credential]
   revision node[:repo][:default][:revision]
-  app_user node[:web_apache][:application_name]
   repository node[:repo][:default][:repository]
-end
-
-template "#{node[:repo][:default][:destination]}/#{node[:ee][:main]}/#{node[:ee][:system_folder]}/expressionengine/config/database.php" do
-  source "database.php.erb"
-  mode 0666
-  owner node[:web_apache][:application_name]
+  migrate false
+  shallow_clone true
+  enable_submodules true
+  user node[:web_apache][:application_name]
   group node[:web_apache][:application_name]
+  before_restart do
+    execute "install_assets" do
+      cwd site_install_dir
+      command "bundle install && rake"
+      #only run if rake file exists
+      only_if { ::File.exists?("#{current_release}/Rakefile") }
+    end
+
+    #Make sure EE permissions are correct
+    bash "set_permissions" do
+      user "root"
+      cwd "#{current_release}"
+      code <<-EOH
+        chmod -R 777 hello/expressionengine/cache
+        chmod -R 777 assets/cache
+        chmod -R 777 assets/templates
+        chmod -R 777 images
+        EOH
+    end
+
+    template "#{current_release}/#{node[:ee][:system_folder]}/expressionengine/config/database.php" do
+      source "database.php.erb"
+      mode 0666
+      owner node[:web_apache][:application_name]
+      group node[:web_apache][:application_name]
+    end
+
+    file "#{current_release}/#{node[:ee][:system_folder]}/expressionengine/config/config.php" do
+      action :touch
+      mode 0666
+      owner node[:web_apache][:application_name]
+      group node[:web_apache][:application_name]
+    end
+  end
+  notifies :restart, "service[apache2]"
 end
 
-file "#{node[:repo][:default][:destination]}/#{node[:ee][:main]}/#{node[:ee][:system_folder]}/expressionengine/config/config.php" do
-  action :touch
-  mode 0666
-  owner node[:web_apache][:application_name]
-  group node[:web_apache][:application_name]
-end
-
-execute "install_assets" do
-  cwd site_install_dir
-  command "bundle install && rake"
-  #only run if rake file exists
-  only_if { ::File.exists?("#{site_install_dir}/Rakefile") }
-end
-
-#Make sure EE permissions are correct
-bash "set_permissions" do
- user "root"
- cwd "#{node[:repo][:default][:destination]}/#{node[:ee][:main]}"
- code <<-EOH
-   chmod -R 777 hello/expressionengine/cache
-   chmod -R 777 assets/cache
-   chmod -R 777 assets/templates
-   chmod -R 777 images
- EOH
-end
-
-service "apache2" do
-  action :reload
-end
 
 rightscale_marker :end
 
