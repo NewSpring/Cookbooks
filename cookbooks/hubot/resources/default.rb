5cb595397266be921cce0f889da57db6dd68f6ec
Drew Delianides <drew@delianides.com>
2013-09-29 11:48:00 -0400

    unrequired room?

--- cookbooks/hubot/resources/default.rb
+++ cookbooks/hubot/resources/default.rb
@@ -6,5 +6,5 @@ end
 actions :run
 
 attribute :url, :kind_of => String, :required => true
-attribute :room, :kind_of => String, :required => true
+attribute :room, :kind_of => String, :required => false
 attribute :message, :kind_of => String, :required => true
