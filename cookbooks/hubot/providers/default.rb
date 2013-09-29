e1c5e5a08a4e49fc2507af6d004725353409560f
Drew Delianides <drew@delianides.com>
2013-09-29 12:12:34 -0400

    more debugging

--- cookbooks/hubot/providers/default.rb
+++ cookbooks/hubot/providers/default.rb
@@ -1,12 +1,12 @@
 action :run do
-  webroom = node[:hubot][:room] || new_resource.room
+  room = node[:hubot][:room] || new_resource.room
   message = new_resource.message
-  url = node[:hubot][:url] || new_resource.url
+  webhook = node[:hubot][:url] || new_resource.url
 
   http_request "send_hubot_message" do
     action :post
-    url url
-    message { :message => "#{message}", :room => "#{webroom}" }
+    url "#{webhook}"
+    message :message => "#{message}", :room => "#{room}"
   end
 end
 
