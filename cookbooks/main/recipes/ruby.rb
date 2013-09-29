d24c5d3ceb88e9b559ab64752286807d3c9af3fb
Drew Delianides <drew@delianides.com>
2013-09-28 22:01:09 -0400

    version locking gems

--- cookbooks/main/recipes/ruby.rb
+++ cookbooks/main/recipes/ruby.rb
@@ -9,16 +9,16 @@ end
 domainatrix.run_action(:install)
 
 execute "install_sass" do
-  command "gem install sass --no-ri --no-rdoc --version '> 3.2.11'"
+  command "gem install sass --no-ri --no-rdoc --version '= 3.2.11'"
   not_if "`which sass`"
 end
 
 execute "install_coffeescript" do
-  command "gem install coffeescript --no-ri --no-rdoc --version '> 2.2.0'"
+  command "gem install coffeescript --no-ri --no-rdoc --version '= 2.2.0'"
   not_if "`which coffee`"
 end
 
 execute "install_rake" do
-  command "gem install rake --no-ri --no-rdoc --version '> 10.1.0'"
+  command "gem install rake --no-ri --no-rdoc --version '= 10.1.0'"
   not_if "`which rake`"
 end
