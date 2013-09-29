7403a47dd2bb6375dba3d5494882f4b986215188
Drew Delianides <drew@delianides.com>
2013-09-28 20:37:14 -0400

    added versions to scripts

--- cookbooks/main/recipes/ruby.rb
+++ cookbooks/main/recipes/ruby.rb
@@ -9,11 +9,11 @@ end
 domainatrix.run_action(:install)
 
 execute "install_sass" do
-  command "gem install sass --no-ri --no-rdoc"
+  command "gem install sass --no-ri --no-rdoc --version '> 3.2.11'"
   not_if "`which sass`"
 end
 
 execute "install_coffeescript" do
-  command "gem install sass --no-ri --no-rdoc"
+  command "gem install coffeescript --no-ri --no-rdoc --version '> 2.2.0'"
   not_if "`which coffee`"
 end
