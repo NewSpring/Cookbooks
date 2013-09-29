e4560601ca265d7f772aef82987eb6a9a4cb5d4b
Drew Delianides <drew@delianides.com>
2013-09-29 12:09:46 -0400

    debugging http request

--- cookbooks/hubot/providers/default.rb
+++ cookbooks/hubot/providers/default.rb
@@ -1,12 +1,12 @@
 action :run do
-  room = node[:hubot][:room] || new_resource.room
+  webroom = node[:hubot][:room] || new_resource.room
   message = new_resource.message
   url = node[:hubot][:url] || new_resource.url
 
   http_request "send_hubot_message" do
     action :post
     url url
-    message :message => "#{message}", :room => "#{room}"
+    message { :message => "#{message}", :room => "#{webroom}" }
   end
 end
 
