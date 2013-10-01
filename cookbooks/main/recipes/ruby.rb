8cb1f2b6584b1c6fd410f8d05db74b6c53bc47a2
Drew Delianides <drew@delianides.com>
2013-10-01 00:03:55 -0400

    few changes

--- cookbooks/main/recipes/ruby.rb
+++ cookbooks/main/recipes/ruby.rb
@@ -21,25 +21,21 @@ domainatrix.run_action(:install)
 sass = gem_package 'sass' do
   action :nothing
   version '3.2.11'
-  options("--no-ri --no-rdoc")
 end
 
 fsevent = gem_package 'rb-fsevent' do
   action :nothing
   version '0.9.3'
-  options("--no-ri --no-rdoc")
 end
 
 coffee = gem_package 'coffee-script' do
   action :nothing
   version '2.2.0'
-  options("--no-ri --no-rdoc")
 end
 
 rake = gem_package 'rake' do
   action :nothing
   version '10.1.0'
-  options("--no-ri --no-rdoc")
 end
 
 sass.run_action(:install)
