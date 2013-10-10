d94576c67a5fcf41b656c3e697479953bcb73c7c
Drew Delianides <drew@delianides.com>
2013-10-10 15:29:39 -0400

    syntax error

--- cookbooks/main/recipes/do_cache_on.rb
+++ cookbooks/main/recipes/do_cache_on.rb
@@ -3,7 +3,7 @@ ruby_block "Turn CE Cache On" do
     file = Chef::Util::FileEdit.new("/var/www/newspring.cc/hello/expressionengine/config/config.php")
     file.search_file_replace_line(
       "$config['ce_cache_off']",
-      "";
+      ""
     )
     file.write_file
   end
