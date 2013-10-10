8a8ee7e793da214bf6b4b9111aea2c1fcae83185
Drew Delianides <drew@delianides.com>
2013-10-10 15:52:18 -0400

    now????

--- cookbooks/main/recipes/do_cache_on.rb
+++ cookbooks/main/recipes/do_cache_on.rb
@@ -1,10 +1,7 @@
 ruby_block "Turn CE Cache On" do
   block do
     file = Chef::Util::FileEdit.new("/var/www/newspring.cc/hello/expressionengine/config/config.php")
-    file.search_file_replace_line(
-      "$config['ce_cache_off'] = 'yes';",
-      " "
-    )
+    file.search_file_delete_line("ce_cache_off")
     file.write_file
   end
 end
