12c58af75281a96b951e351cb73a0ab024463b16
Drew Delianides <drew@delianides.com>
2013-11-11 15:02:18 -0500

    updated default template for apache

--- cookbooks/main/recipes/do_apache_configure.rb
+++ cookbooks/main/recipes/do_apache_configure.rb
@@ -4,8 +4,14 @@ http_port = "80"
 
 # Disable default vhost.
 # See https://github.com/rightscale/cookbooks/blob/master/apache2/definitions/apache_site.rb for the "apache_site" definition.
-apache_site "000-default" do
-  enable false
+# apache_site "000-default" do
+#   enable false
+# end
+
+template "#{node[:apache][:dir]}/sites-available/default" do
+  cookbook "apache2"
+  source "default-site.erb"
+  mode 0644
 end
 
 # Updating apache listen ports configuration
@@ -15,7 +21,7 @@ template "#{node[:apache][:dir]}/ports.conf" do
   variables :apache_listen_ports => http_port
 end
 
-template "/etc/apache2/sites-available/localhost.conf" do
+template "#{node[:apache][:dir]}/sites-available/localhost.conf" do
   source "localhost.conf.erb"
   mode 0644
 end
