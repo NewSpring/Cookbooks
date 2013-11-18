77f54a125f805efef13b8d90d3fa1c834f7bb21b
Drew Delianides <drew@delianides.com>
2013-11-18 15:31:23 -0500

    added redis cookbooks

---
+++ cookbooks/ulimit/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Brian Hatfield"
+maintainer_email "bmhatfield@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures ulimit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "ulimit"
+version          "0.3.2"
+
+recipe "ulimit::default", "Default Recipe"
