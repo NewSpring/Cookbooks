d9c18eb52d351c0c3bca0ee75e14aefed017b6bb
Drew Delianides <drew@delianides.com>
2013-11-20 14:54:42 -0500

    redis firewall

--- cookbooks/main/recipes/do_redis_firewall.rb
+++ cookbooks/main/recipes/do_redis_firewall.rb
@@ -1,8 +1,11 @@
 rightscale_marker :begin
 
-bash "open-redis-port" do
+redis = execute "default" do
   command "iptables -I FWR 4 -i eth1 -p tcp --dport #{node[:redisio][:port]} -j ACCEPT"
+  action :nothing
 end
+
+redis.run_action(:run)
 
 right_link_tag "redis_server:active=true"
 right_link_tag "redis_server:port=#{node[:redisio][:port]}"
