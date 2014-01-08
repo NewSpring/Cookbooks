2aa051cad3874caff007ddc5ad52f970d3715d98
Drew Delianides <drew@delianides.com>
2014-01-08 11:06:18 -0500

    added host setup for private local IP

--- cookbooks/main/recipes/do_vpn_connect.rb
+++ cookbooks/main/recipes/do_vpn_connect.rb
@@ -21,6 +21,17 @@ package "openconnect" do
   action :install
 end
 
+ruby_block "Adding Private IP of AD Server to /etc/hosts" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/hosts")
+    file.insert_line_if_no_match(
+      "cen-dc001.ad.newspring.cc",
+      "10.0.60.10 cen-dc001.ad.newspring.cc"
+    )
+    file.write_file
+  end
+end
+
 package "monit" do
   action :install
 end
