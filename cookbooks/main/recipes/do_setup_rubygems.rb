8b5a8fdcb16eefe72ed8069f121d43168f36ba08
Drew Delianides <drew@delianides.com>
2014-02-10 11:09:41 -0500

    trying to update ruby to 1.9.3

--- cookbooks/main/recipes/do_setup_rubygems.rb
+++ cookbooks/main/recipes/do_setup_rubygems.rb
@@ -1,11 +1,9 @@
 rightscale_marker :begin
 
-package "rubygems" do
-  action :install
-end
-
-package "ruby-bundler" do
-  action :install
+%w{ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev ruby-bundler}.each do |p|
+  package "#{p}" do
+    action :install
+  end
 end
 
 domainatrix = chef_gem "domainatrix" do
