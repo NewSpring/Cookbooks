4d6d3bf5df18cbc0fa5aebfab8ca409a15ce210e
Drew Delianides <drew@delianides.com>
2014-02-18 11:03:13 -0500

    corrected ruby regex syntax

--- cookbooks/expressionengine/recipes/update.rb
+++ cookbooks/expressionengine/recipes/update.rb
@@ -26,34 +26,13 @@ end
 #   node.set[:ee][:env] = "prod"
 # end
 
-ruby_block "Set the Database Hostname" do
+ruby_block "Set the Database Config" do
   block do
     file = Chef::Util::FileEdit.new("#{site_install_dir}/config/config.#{node[:ee][:env]}.php")
-    file.search_file_replace_line("\['hostname'\]", "$env_db['hostname'] = '#{node[:ee][:hostname]}';" )
-    file.write_file
-  end
-end
-
-ruby_block "Set the Database Username" do
-  block do
-    file = Chef::Util::FileEdit.new("#{site_install_dir}/config/config.#{node[:ee][:env]}.php")
-    file.search_file_replace_line("\['username'\]", "$env_db['username'] = '#{node[:ee][:username]}';" )
-    file.write_file
-  end
-end
-
-ruby_block "Set the Database Password" do
-  block do
-    file = Chef::Util::FileEdit.new("#{site_install_dir}/config/config.#{node[:ee][:env]}.php")
-    file.search_file_replace_line("\['password'\]", "$env_db['password'] = '#{node[:ee][:password]}';" )
-    file.write_file
-  end
-end
-
-ruby_block "Set the Database" do
-  block do
-    file = Chef::Util::FileEdit.new("#{site_install_dir}/config/config.#{node[:ee][:env]}.php")
-    file.search_file_replace_line("\['database'\]", "$env_db['database'] = '#{node[:ee][:database]}';" )
+    file.search_file_replace_line(/\['hostname'\]/, "$env_db['hostname'] = '#{node[:ee][:hostname]}';" )
+    file.search_file_replace_line(/\['username'\]/, "$env_db['username'] = '#{node[:ee][:username]}';" )
+    file.search_file_replace_line(/\['password'\]/, "$env_db['password'] = '#{node[:ee][:password]}';" )
+    file.search_file_replace_line(/\['database'\]/, "$env_db['database'] = '#{node[:ee][:database]}';" )
     file.write_file
   end
 end
